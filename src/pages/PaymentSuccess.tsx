d021f1cf6aa19c53cc3eb26e332642c7347b831d
omoritechgod <tguniverse.tech@gmail.com>
2025-08-28 01:55:31 +0100

    Fix PaymentSuccess.tsx import error

--- src/pages/PaymentSuccess.tsx
+++ src/pages/PaymentSuccess.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Link, useSearchParams, useEffect } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Link, useSearchParams } from "react-router-dom";
 import { CheckCircle } from "lucide-react";
 import { serviceOrderApi } from "../services/serviceOrderApi";
 
